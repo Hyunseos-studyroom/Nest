52cade8b35794c9adfac0553cc99adbef43fdf43
정삼복 <68502584+3boku@users.noreply.github.com>
2024-12-16 22:21:25 +0900

    5.2

--- cf_sns/src/posts/posts.controller.ts
+++ cf_sns/src/posts/posts.controller.ts
@@ -1,7 +1,8 @@
 import { Controller, Get } from '@nestjs/common';
 import { PostsService } from './posts.service';
 
-interface Post {
+interface PostModel {
+  id: number;
   author: string;
   title: string;
   content: string;
@@ -9,17 +10,41 @@ interface Post {
   commentCount: number;
 }
 
+let posts: PostModel[] = [
+  {
+    id: 1,
+    author: 'newjeans',
+    title: '뉴진스 민지',
+    content: '메이크업 고치고 있는 민지',
+    likeCount: 12344124,
+    commentCount: 999999,
+  },
+  {
+    id: 2,
+    author: 'newjeans',
+    title: '뉴진스 헤린',
+    content: '고양이 혜린',
+    likeCount: 553223,
+    commentCount: 124124,
+  },
+  {
+    id: 3,
+    author: 'kissoflife',
+    title: '키오프 나띠',
+    content: '춤추는 나띠',
+    likeCount: 124455,
+    commentCount: 345775,
+  },
+];
+
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
+
+  // GET /posts
+  // 모든 posts를 다 가져온다.
   @Get()
-  getHello(): Post {
-    return {
-      author: 'newjeans',
-      title: '뉴진스 민지',
-      content: '민지 개이쁘다',
-      likeCount: 12344124,
-      commentCount: 999999,
-    };
+  getPosts(): PostModel[] {
+    return posts;
   }
 }
