f865f89dfdb72ee70af589faffbf227bfbe34a4f
정삼복 <68502584+3boku@users.noreply.github.com>
2024-12-16 21:59:19 +0900

    5.4

--- cf_sns/src/app.controller.ts
+++ cf_sns/src/app.controller.ts
@@ -1,12 +1,26 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+interface Post {
+  author: string;
+  title: string;
+  content: string;
+  likeCount: number;
+  commentCount: number;
+}
+
+@Controller('posts')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('4')
+  getHello(): Post {
+    return {
+      author: 'newjeans',
+      title: '뉴진스 민지',
+      content: '민지 개이쁘다',
+      likeCount: 12344124,
+      commentCount: 999999,
+    };
   }
 }
