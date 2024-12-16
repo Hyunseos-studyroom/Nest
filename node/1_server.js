c801ce991ad9db93d54e40ce3a66b7b25c3091d9
정삼복 <68502584+3boku@users.noreply.github.com>
2024-12-16 16:34:28 +0900

    3.2

--- node/1_server.js
+++ node/1_server.js
@@ -1,12 +1,23 @@
 const http = require("http");
+const url = require("url");
 
 const host = "localhost";
 // 127.0.0.1 loop back
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.end("<h1>Hello World</h1>");
+    const path = url.parse(req.url).pathname;
+
+    if (path === "/") {
+        res.writeHead(200, { "content-type": "text/html" });
+        res.end("<h1>Home!</h1>");
+    } else if (path === "/post") {
+        res.writeHead(200, { "content-type": "text/html" });
+        res.end("<h1>Post!</h1>");
+    } else {
+        res.writeHead(404, { "content-type": "text/html" });
+        res.end("<h1>404!</h1>");
+    }
 });
 
 server.listen(port, host, () => {
